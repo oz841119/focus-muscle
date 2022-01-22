9d2a4b63f2e4b4acd0a457bff3309c64640f24c0
oz841119 <oz841119@gmail.com>
2022-01-22 18:49:46 +0800

    none

--- src/components/MuscleBox.vue
+++ src/components/MuscleBox.vue
@@ -10,9 +10,6 @@
           <a class="actions" :href="'http://google.com.tw/search?q='+item"  target="_blank">{{item}}</a>
           <span class="good" @click="submitGood(item)" :class="{alreadySub: alreadySubmitted.includes(item)}">讚</span>
         </div>
-        <!-- <a class="actions mt8" v-for="(item,index) in trainingAction" :key="index" :href="'http://google.com.tw/search?q='+item"  target="_blank">
-          {{item}}
-        </a> -->
         <div class="ps">
           <div>複合性動作皆有輔助肌群共同發力<br/>本列表提供能對該部位大量刺激的訓練動作<br/>當角度或其他部位控制不同時 可能會有不同的壓力模式<br/>解剖圖、肌群名稱及訓練動作具有非正確性 待日後修正<br/></div>
           <div class="advices cp">提供意見(建構中)</div>
@@ -34,7 +31,6 @@ export default {
   },
   mounted(){
     this.judgmentMuscleName(this._props.muscleName)
-    // window.localStorage.clear()
     this.initLocalStorage()
     console.log(window.localStorage);
   },
@@ -70,18 +66,11 @@ export default {
 <style scoped>
   .muscleBox {
     position: absolute;
-    /* bottom: 10%; */
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     width: 60vw;
-    /* height: 70vh; */
     max-height: 60vh;
-    /* background-color: #fff;
-    background: #F8F8F8; 
-    border-radius: 8px;
-    border: solid #BDBDBD 0; 
-    box-shadow: 6px 5px 22px rgba(0, 0, 0, .1); */
     overflow: auto;
 
     background: rgba(0, 0, 0, .1);
