37e59e3ae0ff441d071351624ee40de70c286c2a
oz841119 <oz841119@gmail.com>
2022-02-07 19:37:04 +0800

    增加驗證input

--- src/components/SuggestionBox.vue
+++ src/components/SuggestionBox.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="suggestionBox mt8">
-    <input type="text" class="mr8" placeholder="輸入您想建議的動作名稱" v-model="advice">
-    <input type="text" class="mr8 mt8" placeholder="提供人(可選)" v-model="provider">
-    <input type="submit" class="mr8" :value="submitVaule" @click="submit()" :disabled="disabled">
+    <input type="text" class="mr8" placeholder="輸入您想建議的動作名稱" v-model.trim="advice" :disabled="disabled">
+    <input type="text" class="mr8" placeholder="提供人(可選)" v-model="provider" :disabled="disabled">
+    <input type="submit" class="submit mr8" :value="submitVaule" @click="submit()" :disabled="disabled">
     <transition name="fade">
       <span v-if="isSuccess">{{message}}</span>
     </transition>
@@ -19,16 +19,28 @@ export default {
       disabled: false,
       submitVaule: '送出',
       isSuccess: false,
-      message: '送出成功! 審核後將會新增'
+      message: '送出成功! 審核後將會新增',
+      nameError: false,
+      nameErrorMsg: ''
     }
   },
   methods: {
     submit() {
+      const nameNumReg = /^[0-9]*$/
+      console.log(nameNumReg.test(this.advice));
+
       if(!this.advice) {
         this.isSuccess = true
         this.message = "請輸入動作名稱"
         return
       }
+
+      if(nameNumReg.test(this.advice) || this.advice.length <= 1) {
+        this.isSuccess = true
+        this.message = "請輸入正確的動作名稱"
+        return
+      }
+      
       this.disabled = !this.disabled
       this.submitVaule = '送出中'
       let xhr = new XMLHttpRequest
