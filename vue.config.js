9e53d4188438c287f7f704405adeb0928ddfc19e
oz841119 <oz841119@gmail.com>
2022-02-07 16:59:57 +0800

    打包時輸出與資料夾同名的資料夾

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+var dirNameArray
+if(__dirname.indexOf('\\')!=-1){
+    dirNameArray = __dirname.split('\\');
+}else if(__dirname.indexOf('/')!=-1){
+    dirNameArray = __dirname.split('/');
+}
+
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/focus-muscle/' 
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/focus-muscle/'  : '/',
+  outputDir: dirNameArray[dirNameArray.length-1]
 }
